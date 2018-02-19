c870e919ebd33a93ae91b2a39155f0551d597aa5
Martin Rustler <martin.rustler@googlemail.com>
2018-02-19 12:15:14 +0100

    Initial commit

---
+++ MinTempSpread/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MinTempSpread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawData = File.ReadAllText(@".\weather.dat");
+
+            var temperatureData = TemperatureData.ExtractFromRawData(rawData);
+            Console.WriteLine($"Read {temperatureData.Count()} lines of weather data.");
+
+            Func<TemperatureData, int> temperatureSpread = data => data.MaxTemp - data.MinTemp;
+            var smallestSpread = temperatureData.OrderBy(temperatureSpread).First();
+
+            Console.WriteLine("Day with the smallest temperature spread:");
+            Console.WriteLine($"{smallestSpread.Day} (with a temperature spread of {temperatureSpread(smallestSpread)}°F)");
+        }
+    }
+}
---
+++ MinTempSpread/TemperatureData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MinTempSpread
+{
+    internal class TemperatureData
+    {
+        public int Day { get; private set; }
+        public int MaxTemp { get; private set; }
+        public int MinTemp { get; private set; }
+
+        private TemperatureData(int day, int maxTemp, int minTemp)
+        {
+            Day = day;
+            MaxTemp = maxTemp;
+            MinTemp = minTemp;
+        }
+
+        public static IEnumerable<TemperatureData> ExtractFromRawData(string rawData)
+        {
+            var pattern = @"^\s*(\d+)\s+(\d+)\**\s+(\d+)\**";
+            var regex = new Regex(pattern, RegexOptions.Multiline);
+            var matches = regex.Matches(rawData);
+
+            foreach (Match match in matches)
+            {
+                var day = int.Parse(match.Groups[1].Value);
+                var maxTemp = int.Parse(match.Groups[2].Value);
+                var minTemp = int.Parse(match.Groups[3].Value);
+
+                yield return new TemperatureData(day, maxTemp, minTemp);
+            }
+        }
+    }
+}
